d02c2db6d9194f2b23179c7523613e993b7c7d9a
mohamedmounir2425 <mohamed.mounir2425@gmail.com>
2024-06-22 20:24:35 +0300

    setup the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarAuthComponent } from './components/navbar-auth/navbar-auth.component';
+import { NavbarBlankComponent } from './components/navbar-blank/navbar-blank.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarAuthComponent,
+    NavbarBlankComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'E-commerce_app_route';
