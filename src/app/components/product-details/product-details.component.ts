d8c8fad52d388649b03375948e396f5a3b60e0ec
mohamedmounir2425 <mohamed.mounir2425@gmail.com>
2024-06-25 22:53:48 +0300

    add btn remove-add in product details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -3,17 +3,22 @@ import { ProductsService } from './../../services/products.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { CartService } from '../../services/cart.service';
+import { ToastrService } from 'ngx-toastr';
+import { AddRemoveCartComponent } from '../add-remove-cart/add-remove-cart.component';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [CurrencyPipe, CarouselModule],
+  imports: [CurrencyPipe, CarouselModule, AddRemoveCartComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent implements OnInit {
   id!: string;
   product: any;
+  cartCount: number = 0;
+  isLoading: boolean = false;
   productOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
@@ -29,7 +34,9 @@ export class ProductDetailsComponent implements OnInit {
   };
   constructor(
     private route: ActivatedRoute,
-    private _ProductsService: ProductsService
+    private _ProductsService: ProductsService,
+    private toastr: ToastrService,
+    private _CartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -46,6 +53,40 @@ export class ProductDetailsComponent implements OnInit {
       },
       error: (err) => {
         console.error(err);
+      },
+    });
+    this._CartService.getCart().subscribe({
+      next: ({ data }) => {
+        console.log(data);
+        this.cartCount =
+          data.products.find((p: any) => p.product.id == this.product.id)
+            ?.count || 0;
+        // this.product = data;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  addToCart(productId: string) {
+    this.isLoading = true;
+    this._CartService.addToCart(productId).subscribe({
+      next: (res) => {
+        console.log(res);
+        if (res.status === 'success') {
+          this.cartCount = res.data.products.find(
+            (p: any) => p.product === this.product._id
+          ).count;
+        }
+        this.isLoading = false;
+        this.toastr.success(res.message, undefined, {
+          timeOut: 3000,
+        });
+      },
+      error: (err) => {
+        console.log(err);
+        this.isLoading = false;
       },
     });
   }
