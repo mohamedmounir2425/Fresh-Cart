93153708c2eca2334377ccccd7d883b6a5f657be
mohamedmounir2425 <mohamed.mounir2425@gmail.com>
2024-07-02 07:02:34 +0300

    create loading products

---
+++ src/app/components/products/loading-products/loading-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-products.component.html',
+  styleUrl: './loading-products.component.scss'
+})
+export class LoadingProductsComponent {
+
+}
